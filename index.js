2a4284f7634c8b04d7b7857f34a5e1a4e9d4992a
Paradise9898 <bakuwork08@gmail.com>
2021-12-27 03:28:48 +0300

    init

--- index.js
+++ index.js
@@ -24,7 +24,28 @@ app.use(express(__dirname + '/views'))
   })
   
   app.post('/api', function(req,res){
-      console.log(req);
+      const v = req.body
+      console.log(v);
+
+      async function postData(url = '', data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+      }
+
+      
+      postData('', v)
+        .then(data => {
+          console.log(data); // JSON data parsed by `data.json()` call
+        });
+      
   })
 
 
