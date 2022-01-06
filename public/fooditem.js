5dc069acc5f847ed1160c5fe5f7d4aada386f82f
paradise9898 <spookyambassador98@gmail.com>
2022-01-07 02:04:52 +0300

    init

---
+++ public/fooditem.js
@@ -0,0 +1,34 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ambur Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: './public/images/biryani/Ambur-Chicken-Biryani.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/Chicken-Biryani-hyd.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'images/biryani/egg-biryani.jpeg',
+    quantity: 1
+},
+
+]
+
+export {foodItem};
