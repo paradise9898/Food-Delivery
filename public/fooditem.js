e45c98561b29244ed18890cf7bc165c79e6dd64c
paradise9898 <spookyambassador98@gmail.com>
2022-01-07 04:01:50 +0300

    init

--- public/fooditem.js
+++ public/fooditem.js
@@ -1,31 +1,66 @@
 const foodItem= [
-    {
-    id: 1,
-    name: 'Ambur Biryani',
-    category : 'biryani',
-    rating : 4.3,
-    price: 13,
-    img: './public/images/biryani/Ambur-Chicken-Biryani.jpg',
-    quantity: 1
-},
-
-
+  
 {
-    id: 2,
-    name: 'Hyderabadi Biryani',
-    category : 'biryani',
+    id: 30,
+    name: 'Momos',
+    category : 'chinese',
     rating : 4.3,
-    price: 15,
-    img: 'images/biryani/Chicken-Biryani-hyd.jpg',
+    price: 8,
+    img: './public/images/chinese/cabbage-momos-.jpg',
     quantity: 1
 },
 {
-    id: 3,
-    name: 'Egg Biryani',
-    category : 'biryani',
+    id: 31,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
     rating : 4.3,
-    price: 18,
-    img: './public/images/biryani/egg-biryani.jpeg',
+    price: 7,
+    img: './public/images/chinese/ChickenManchurian.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chili Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: './public/images/chinese/Chili-Chicken.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Chowmein',
+    category : 'chinese',
+    rating : 4.3,
+    price: 16,
+    img: './public/images/chinese/chowmin.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 14,
+    img: './public/images/chinese/spring-rolls.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: './public/images/chinese/szechuan-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: './public/images/chinese/veg-fried-rice.jpg',
     quantity: 1
 },
 
